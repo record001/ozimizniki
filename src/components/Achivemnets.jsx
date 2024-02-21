83b2fc6e40c16253077e005fea900828a68d27f1
record001 <umarkhidirboev@gmail.com>
2024-02-21 18:40:43 +0500

    policy changed

--- src/components/Achivemnets.jsx
+++ src/components/Achivemnets.jsx
@@ -15,7 +15,7 @@ function Achivemnets() {
               <div className="round">
                  <img src={rocket} alt="" />
               </div>
-              <h4>1- bosqich: <br />1 000 000 so'mlik sarmoya kiritasiz va  dastlabki kitoblar to'plamiga ega bo'lasiz</h4>
+              <h4>1- bosqich: <br />Siz 1 000 000 so'm sarmoya hisobiga  dastlabki kitoblar to'plamiga ega bo'lasiz</h4>
             </div>
             <img className='arrow' src={top} alt="" />
             <div className="achivement__card">
@@ -32,7 +32,7 @@ function Achivemnets() {
               <img src={track} alt="" />
 
               </div>
-              <h4>5- bosqich: <br />  1 500 000 so'mlik kitoblar to'plamini olasiz</h4>
+              <h4>5- bosqich: <br />Siz  1 500 000 so'mlik kitoblar to'plamini olasiz</h4>
             </div>
             <img className='arrow'  src={top} alt="" />
 
@@ -40,7 +40,10 @@ function Achivemnets() {
               <div className="round">
               <img src={human} alt="" />
               </div>
-              <h4>6- bosqich: <br /> Bir kishilik "Umra ziyorati"   yo'llanmasi va loyiha hisobidan reinvest </h4>
+              <h4>6- bosqich: <br />  Hohishingizga qarab: <br />
+               1. "Umra ziyorati" <br />
+               2.  10 mln so'm hamda 3 mln so'mlik kitoblar to'plami. <br />
+               Har ikki holatda ham reinvest </h4>
             </div>
           </div>    
       </div>     
